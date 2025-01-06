2ba5d972bb4209cd978553b31a306267ed173a13
Beau Taapken <beau@lioncode.nl>
2025-01-06 21:00:12 +0100

    can rotate now

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.update(dt)
+        player.draw(screen)
+        dt = clock.tick(165) / 1000
         pygame.display.flip()
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
